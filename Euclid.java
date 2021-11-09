efc18c971a0e3a7b0e922d8f86d1e4bd27b9bd32
saeedneoteric78@gmail.com <(saeedneoteric786@gmail.com)>
2021-11-09 19:50:56 +0500

    Euclid Algo

---
+++ Euclid.java
@@ -0,0 +1,18 @@
+public class Euclid{
+public static int gcd(int p,int q)
+{
+	while(q!=0)
+{
+	int temp=q;
+	q=p%q;
+	p=temp;
+}
+return p;
+}
+  public static void main(String[] args) {
+    int p=Integer.parseInt(args[0]);
+    int q=Integer.parseInt(args[1]);
+    int d=gcd(p,q);
+    System.out.println("GCD of"+p+"and"+q+"="+d);
+  }
+}
